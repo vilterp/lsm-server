9e20d9b1cf9956419713a60f1347b32904c9eae3
Pete Vilter <pete.vilter@gmail.com>
2021-05-11 13:32:49 -0400

    hook up LSM to server

--- main.go
+++ main.go
@@ -5,25 +5,21 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"sync"
+	"path"
 
 	"github.com/vilterp/kv-server/server"
 )
 
 type kvServer struct {
-	contents map[string]string
-	mutex    sync.Mutex
-
-	wal *server.KVFile
+	lsm *server.LSM
 
 	mux http.ServeMux
 }
 
-func NewKVServer(walWriter *server.KVFile) *kvServer {
+func NewKVServer(lsm *server.LSM) *kvServer {
 	s := &kvServer{
-		wal:      walWriter,
-		contents: map[string]string{},
-		mux:      http.ServeMux{},
+		lsm: lsm,
+		mux: http.ServeMux{},
 	}
 	s.mux.HandleFunc("/set", s.handleSet)
 	s.mux.HandleFunc("/get", s.handleGet)
@@ -31,9 +27,6 @@ func NewKVServer(walWriter *server.KVFile) *kvServer {
 }
 
 func (k *kvServer) handleSet(w http.ResponseWriter, req *http.Request) {
-	k.mutex.Lock()
-	defer k.mutex.Unlock()
-
 	// validate the input: reject if more than one value for a key
 	if len(req.URL.Query()) != 1 {
 		http.Error(w, fmt.Sprintf("expecting exactly one key"), http.StatusBadRequest)
@@ -48,60 +41,35 @@ func (k *kvServer) handleSet(w http.ResponseWriter, req *http.Request) {
 
 	query := req.URL.Query()
 	for key, value := range query {
-		k.contents[key] = value[0]
-		if err := k.wal.AppendKVPair([]byte(key), []byte(value[0])); err != nil {
-			http.Error(w, fmt.Sprintf("error writing key: %s", err), http.StatusInternalServerError)
+		if err := k.lsm.Put([]byte(key), []byte(value[0])); err != nil {
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			log.Println("error handling set:", err)
 			return
 		}
 	}
 }
 
 func (k *kvServer) handleGet(w http.ResponseWriter, req *http.Request) {
-	k.mutex.Lock()
-	defer k.mutex.Unlock()
-
 	keys := req.URL.Query()["key"]
 	if len(keys) != 1 {
 		http.Error(w, fmt.Sprintf("should pass exactly one keys"), http.StatusBadRequest)
 		return
 	}
 
-	value, ok := k.contents[keys[0]]
+	value, ok, err := k.lsm.Get([]byte(keys[0]))
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		log.Println("error handling get:", err)
+		return
+	}
 	if !ok {
 		http.NotFound(w, req)
 		return
 	}
 
-	_, err := fmt.Fprintln(w, value)
-	if err != nil {
+	if _, err := fmt.Fprintln(w, value); err != nil {
 		log.Println("error writing response:", err)
 	}
-}
-
-func (k *kvServer) loadFromWAL() (int, error) {
-	k.mutex.Lock()
-	defer k.mutex.Unlock()
-
-	read := 0
-
-	reader, err := k.wal.GetReader()
-	if err != nil {
-		return 0, err
-	}
-	for {
-		kvPair, err := reader.Next()
-		if err != nil {
-			return 0, err
-		}
-		if kvPair == nil {
-			break
-		}
-
-		// TODO: don't stringify
-		k.contents[string(kvPair.Key)] = string(kvPair.Value)
-		read++
-	}
-	return read, nil
 }
 
 func (k *kvServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
@@ -120,22 +88,21 @@ func main() {
 	if !found {
 		port = "9999"
 	}
-	file, found := os.LookupEnv("FILE")
+	dataDir, found := os.LookupEnv("DATA_DIR")
 	if !found {
-		file = "wal.kv"
+		dataDir = "data"
 	}
 
 	// construct server
-	walFile, err := server.NewKVFile(file)
+	walFile, err := server.NewKVFile(path.Join(dataDir, "wal.kv.gob"))
 	if err != nil {
-		log.Fatal("error opening wal:", err)
+		log.Fatal("error opening wal: ", err)
 	}
-	serv := NewKVServer(walFile)
-	read, err := serv.loadFromWAL()
+	lsm, err := server.NewLSM(walFile, dataDir)
 	if err != nil {
-		log.Fatal("error reading log:", err)
+		log.Fatal("error creating LSM: ", err)
 	}
-	log.Printf("read %d keys on startup", read)
+	serv := NewKVServer(lsm)
 
 	log.Printf("listening on %s:%s", host, port)
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%s", host, port), serv); err != nil {
